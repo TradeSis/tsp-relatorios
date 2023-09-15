67bc4220e1be7a40d659772ae0e90d1c2b75f63a
Helio Tradesis <helio@tradesis.com.br>
2023-09-15 16:11:07 -0300

    padronizado conexao
    conexao usará functions, e parametros progress pelo config.php

--- conexao.php
+++ conexao.php
@@ -1,20 +1,14 @@
 <?php
-// Helio 26042023 - conexao agora pega dados do ../config.php, e vai para versao
-// helio 01022023 criado funcao defineConexao API e MySQl com aprametros locais
-// helio 01022023 altereado para include_once
-// helio 31012023 - include database/api
-// helio 26012023 16:16
+
 if (session_status() === PHP_SESSION_NONE) {
 	session_start();
 }
-
 include_once __DIR__."/../config.php";
 
 include_once(ROOT.'/sistema/database/mysql.php');
 include_once(ROOT.'/sistema/database/api.php');
-// helio 26042023
+
 include_once(ROOT.'/sistema/database/functions.php');
 include_once(ROOT.'/sistema/database/email.php');
-
 
 ?>
